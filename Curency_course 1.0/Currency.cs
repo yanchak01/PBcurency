a095fd65dd74cedb95fc9943663a14f49bd596b0
Yanchak Andrii <a.yanchak01@gmail.com>
2018-10-04 23:06:44 +0300

    //add solution

---
+++ Curency_course 1.0/Currency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curency_course_1._0
+{
+    public class Currency
+    {
+        public string baseCurrency { get; set; }
+
+        public string  date { get; set; }
+
+        public string Bank { get; set; }
+
+        public string baseCurrencyLit { get;set;}
+
+        public List<exchangeRate> exchangeRate { get; set; }
+    }
+}
